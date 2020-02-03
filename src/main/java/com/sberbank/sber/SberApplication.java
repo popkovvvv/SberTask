9b36fc074fbfe96059ce74178b19b66c56d8f063
popkovvvv <popkovvvv@mail.ru>
2020-02-03 19:50:46 +0300

    Initial commit

---
+++ src/main/java/com/sberbank/sber/SberApplication.java
@@ -0,0 +1,11 @@
+package com.sberbank.sber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SberApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sberbank/sber/SberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sberbank.sber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
